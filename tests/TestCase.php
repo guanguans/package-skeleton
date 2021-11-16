fe2f95bab45c3f9f733d8602ae9c320a5e7926ac
yaozm <ityaozm@gmail.com>
2021-11-16 11:06:19 +0800

    Update the namespace of the test

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -8,7 +8,7 @@
  * This source file is subject to the MIT license that is bundled.
  */
 
-namespace Guanguans\PackageSkeleton\Tests;
+namespace Guanguans\PackageSkeletonTests;
 
 use DMS\PHPUnitExtensions\ArraySubset\ArraySubsetAsserts;
 
