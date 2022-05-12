f241b4b1a542b526478367f2c3ba5d9f30b12f85
yaozm <ityaozm@gmail.com>
2022-05-12 11:23:15 +0800

    Add kylekatarnls/update-helper

---
+++ src/UpdateHelper.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * This file is part of the guanguans/package-skeleton.
+ *
+ * (c) guanguans <ityaozm@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled.
+ */
+
+namespace Guanguans\PackageSkeleton;
+
+use UpdateHelper\UpdateHelperInterface;
+
+class UpdateHelper implements UpdateHelperInterface
+{
+    public function check(\UpdateHelper\UpdateHelper $helper)
+    {
+        $helper->write('Package update checking...');
+        $helper->write('Package update checking done.');
+    }
+}
