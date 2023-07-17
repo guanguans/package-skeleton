eaaaa8102812a83010e17acecc91424a0f55c169
yaozm <ityaozm@gmail.com>
2023-07-17 15:31:11 +0800

    feat(ReleaseWorker): Add UpdateChangelogReleaseWorker
    - Add `UpdateChangelogReleaseWorker` to update `CHANGELOG.md` with new release information
    - Run `conventional-changelog` command to generate changelog
    - Add `CHANGELOG.md` to git index
    - Reset changes made to `CHANGELOG.md`

--- monorepo-builder.php
+++ monorepo-builder.php
@@ -10,7 +10,7 @@ declare(strict_types=1);
  * This source file is subject to the MIT license that is bundled.
  */
 
-use Symplify\MonorepoBuilder\ComposerJsonManipulator\ValueObject\ComposerJsonSection;
+use Guanguans\PackageSkeleton\UpdateChangelogReleaseWorker;
 use Symplify\MonorepoBuilder\Config\MBConfig;
 use Symplify\MonorepoBuilder\Release\ReleaseWorker\AddTagToChangelogReleaseWorker;
 use Symplify\MonorepoBuilder\Release\ReleaseWorker\PushNextDevReleaseWorker;
@@ -20,41 +20,10 @@ use Symplify\MonorepoBuilder\Release\ReleaseWorker\SetNextMutualDependenciesRele
 use Symplify\MonorepoBuilder\Release\ReleaseWorker\TagVersionReleaseWorker;
 use Symplify\MonorepoBuilder\Release\ReleaseWorker\UpdateBranchAliasReleaseWorker;
 use Symplify\MonorepoBuilder\Release\ReleaseWorker\UpdateReplaceReleaseWorker;
-use Symplify\MonorepoBuilder\ValueObject\Option;
 
 return static function (MBConfig $mbConfig): void {
-    // default: "<major>.<minor>-dev"
-    $mbConfig->packageAliasFormat('<major>.<minor>-dev');
+    require __DIR__.'/vendor/autoload.php';
     $mbConfig->defaultBranch('main');
-
-    // // where are the packages located?
-    // $mbConfig->packageDirectories([__DIR__.'/packages']);
-    //
-    // // how to skip packages in loaded directories?
-    // $mbConfig->packageDirectoriesExcludes([__DIR__.'/packages/secret-package']);
-    //
-    // // "merge" command related
-    // // what extra parts to add after merge?
-    // $mbConfig->dataToAppend([
-    //     ComposerJsonSection::CONFIG => [
-    //         'apcu-autoloader' => true,
-    //         'classmap-authoritative' => false,
-    //         'optimize-autoloader' => true,
-    //         'preferred-install' => 'dist',
-    //         'sort-packages' => true,
-    //     ],
-    // ]);
-    //
-    // $mbConfig->dataToRemove([
-    //     ComposerJsonSection::REQUIRE => [
-    //         // the line is removed by key, so version is irrelevant, thus *
-    //         'phpunit/phpunit' => '*',
-    //     ],
-    //     ComposerJsonSection::REPOSITORIES => [
-    //         // this will remove all repositories
-    //         Option::REMOVE_COMPLETELY,
-    //     ],
-    // ]);
 
     /**
      * release workers - in order to execute
@@ -65,10 +34,11 @@ return static function (MBConfig $mbConfig): void {
         // UpdateReplaceReleaseWorker::class,
         // SetCurrentMutualDependenciesReleaseWorker::class,
         // AddTagToChangelogReleaseWorker::class,
+        UpdateChangelogReleaseWorker::class,
         TagVersionReleaseWorker::class,
         PushTagReleaseWorker::class,
         // SetNextMutualDependenciesReleaseWorker::class,
         // UpdateBranchAliasReleaseWorker::class,
-        PushNextDevReleaseWorker::class,
+        // PushNextDevReleaseWorker::class,
     ]);
 };
