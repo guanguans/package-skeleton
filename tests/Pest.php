bb8eccd59eae2f7dbc08790b971ee7aff77e684e
yaozm <ityaozm@gmail.com>
2023-07-23 18:23:57 +0800

    chore(tests): update Pest.php
      - Update namespace in Pest.php
      - Add beforeAll, beforeEach, afterEach, and afterAll hooks
      - Extend expectations with custom methods
      - Add functions: class_namespace() and fixtures_path()
      - Update comments in Pest.php

--- tests/Pest.php
+++ tests/Pest.php
@@ -10,16 +10,33 @@ declare(strict_types=1);
  * This source file is subject to the MIT license that is bundled.
  */
 
-namespace Guanguans\PackageSkeletonTests;
-
+use Guanguans\PackageSkeletonTests\TestCase;
 use Pest\Expectation;
 
-uses(LaravelTestCase::class)
+uses(TestCase::class)
+    ->beforeAll(function (): void {
+    })
     ->beforeEach(function (): void {
     })
-    ->in(__DIR__);
+    ->afterEach(function (): void {
+    })
+    ->afterAll(function (): void {
+    })
+    ->in(__DIR__.'/Feature', __DIR__.'/Unit');
+/*
+|--------------------------------------------------------------------------
+| Expectations
+|--------------------------------------------------------------------------
+|
+| When you're writing tests, you often need to check that values meet certain conditions. The
+| "expect()" function gives you access to a set of "expectations" methods that you can use
+| to assert different things. Of course, you may extend the Expectation API at any time.
+|
+ */
 
-expect()->extend('assert', function (\Closure $assertions): Expectation {
+expect()->extend('toBeOne', fn () => $this->toBe(1));
+
+expect()->extend('assert', function (Closure $assertions): Expectation {
     $assertions($this->value);
 
     return $this;
@@ -32,3 +49,31 @@ expect()->extend('between', function (int $min, int $max): Expectation {
 
     return $this;
 });
+
+/*
+|--------------------------------------------------------------------------
+| Functions
+|--------------------------------------------------------------------------
+|
+| While Pest is very powerful out-of-the-box, you may have some testing code specific to your
+| project that you don't want to repeat in every file. Here you can also expose helpers as
+| global functions to help you to reduce the number of lines of code in your test files.
+|
+ */
+
+/**
+ * @param object|string $class
+ *
+ * @throws \ReflectionException
+ */
+function class_namespace($class): string
+{
+    $class = is_object($class) ? get_class($class) : $class;
+
+    return (new ReflectionClass($class))->getNamespaceName();
+}
+
+function fixtures_path(string $path = ''): string
+{
+    return __DIR__.'/Fixtures'.($path ? \DIRECTORY_SEPARATOR.$path : $path);
+}
