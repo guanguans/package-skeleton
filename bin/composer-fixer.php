0315723022c7f3df55b06278cb4dbf5ff397d9c5
yaozm <ityaozm@gmail.com>
2023-11-30 11:03:48 +0800

    Fix(bin/composer-fixer.php): Update composer file
    - Use SplFileInfo instead of Symfony\Component\Finder\SplFileInfo
    - Update the composer file path in the note
    - Update the composer file path in the update message
    - Update the composer file path in the note after updating
    - Update the composer file path in the update message after updating

--- bin/composer-fixer.php
+++ bin/composer-fixer.php
@@ -15,6 +15,7 @@ use Symfony\Component\Console\Input\ArgvInput;
 use Symfony\Component\Console\Output\ConsoleOutput;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\Finder\Finder;
+use Symfony\Component\Finder\SplFileInfo;
 use Symfony\Component\Process\Process;
 
 require __DIR__.'/../vendor/autoload.php';
@@ -30,14 +31,13 @@ collect(
         ->depth(0)
         ->files()
 )
-    ->map(static fn (Symfony\Component\Finder\SplFileInfo $splFileInfo, $composerFile): string => realpath($composerFile))
-    ->each(static function ($composerFile): void {
+    ->each(static function (SplFileInfo $splFileInfo): void {
         /** @noinspection PhpUnhandledExceptionInspection */
-        collect(json_decode(file_get_contents($composerFile), true, 512, JSON_THROW_ON_ERROR))
+        collect(json_decode($splFileInfo->getContents(), true, 512, JSON_THROW_ON_ERROR))
             ->only(['require', 'require-dev'])
-            ->each(static function ($packagist, $env) use ($composerFile): void {
+            ->each(static function ($packagist, $env) use ($splFileInfo): void {
                 $symfonyStyle = new SymfonyStyle(new ArgvInput(), new ConsoleOutput());
-                $symfonyStyle->note("The composer file($composerFile) $env updating...");
+                $symfonyStyle->note(sprintf('The composer file(%s) %s updating...', $splFileInfo->getRealPath(), $env));
 
                 $hydratedPackagist = collect($packagist)
                     ->filter(static fn ($version, $package) => ! in_array(
@@ -52,7 +52,7 @@ collect(
                     ->map(static fn ($version, $package) => "$package:'*'")
                     ->implode(' ');
                 if (empty($hydratedPackagist)) {
-                    $symfonyStyle->note("The composer file($composerFile) $env nothing to update.");
+                    $symfonyStyle->note(sprintf('The composer file(%s) %s nothing to update.', $splFileInfo->getRealPath(), $env));
                     $symfonyStyle->newLine();
 
                     return;
@@ -63,12 +63,12 @@ collect(
                     ->when('require-dev' === $env, static fn (Illuminate\Support\Stringable $command) => $command->append(' --dev'))
                     ->__toString();
                 $symfonyStyle->note($command);
-                Process::fromShellCommandline($command, dirname($composerFile), ['COMPOSER_MEMORY_LIMIT' => -1])
+                Process::fromShellCommandline($command, $splFileInfo->getPath(), ['COMPOSER_MEMORY_LIMIT' => -1])
                     ->mustRun(static function ($type, $buffer) use ($symfonyStyle): void {
                         $symfonyStyle->write($buffer);
                     });
 
-                $symfonyStyle->note("The composer file($composerFile) $env updated.");
+                $symfonyStyle->note(sprintf('The composer file(%s) %s updated.', $splFileInfo->getRealPath(), $env));
                 $symfonyStyle->newLine();
             });
     });
