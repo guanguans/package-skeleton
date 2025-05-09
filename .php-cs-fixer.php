eaa6e726c46df6fa0e4e06bb9306451386461ee0
guanguans <ityaozm@gmail.com>
2025-05-09 19:26:57 +0800

    build(vendor-bin/laravel): Remove unused dependencies from composer.json
    - Remove multiple outdated and unused packages
    - Clean up the `require` and `require-dev` sections for optimization
    - This update reduces the number of dependencies for better performance and maintenance

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -11,25 +11,71 @@ declare(strict_types=1);
  * @see https://github.com/guanguans/package-skeleton
  */
 
-use Ergebnis\License;
-use Ergebnis\PhpCsFixer\Config;
+use Ergebnis\License\Holder;
+use Ergebnis\License\Range;
+use Ergebnis\License\Type\MIT;
+use Ergebnis\License\Url;
+use Ergebnis\License\Year;
+use Ergebnis\PhpCsFixer\Config\Factory;
+use Ergebnis\PhpCsFixer\Config\Fixers;
+use Ergebnis\PhpCsFixer\Config\Rules;
+use Ergebnis\PhpCsFixer\Config\RuleSet\Php80;
+use Ergebnis\PhpCsFixer\Config\RuleSet\Php83;
 use PhpCsFixer\Finder;
+use PhpCsFixer\Fixer\DeprecatedFixerInterface;
+use PhpCsFixerCustomFixers\Fixer\AbstractFixer;
 
-$license = License\Type\MIT::text(
-    __DIR__.'/LICENSE',
-    License\Range::since(
-        License\Year::fromString('2021'),
-        new DateTimeZone('Asia/Shanghai'),
-    ),
-    License\Holder::fromString('guanguans<ityaozm@gmail.com>'),
-    License\Url::fromString('https://github.com/guanguans/package-skeleton'),
-);
+return Factory::fromRuleSet(Php80::create()
+    ->withHeader(
+        (static function (): string {
+            $mit = MIT::text(
+                __DIR__.'/LICENSE',
+                Range::since(
+                    Year::fromString('2021'),
+                    new DateTimeZone('Asia/Shanghai'),
+                ),
+                Holder::fromString('guanguans<ityaozm@gmail.com>'),
+                Url::fromString('https://github.com/guanguans/package-skeleton'),
+            );
 
-$license->save();
+            $mit->save();
 
-$ruleSet = Config\RuleSet\Php74::create()
-    ->withHeader($license->header())
-    ->withRules(Config\Rules::fromArray([
+            return $mit->header();
+        })()
+    )
+    ->withCustomFixers(Fixers::fromFixers(...$phpCsFixerCustomFixers = array_filter(
+        iterator_to_array(new PhpCsFixerCustomFixers\Fixers),
+        static fn (AbstractFixer $fixer): bool => !$fixer instanceof DeprecatedFixerInterface
+            && !\array_key_exists($fixer->getName(), Php83::create()->rules()->toArray())
+    )))
+    // ->withRules(Rules::fromArray(array_reduce(
+    //     $phpCsFixerCustomFixers,
+    //     static function (array $rules, AbstractFixer $fixer): array {
+    //         if (
+    //             \in_array(
+    //                 $fixer->getName(),
+    //                 [
+    //                     'PhpCsFixerCustomFixers/comment_surrounded_by_spaces',
+    //                     'PhpCsFixerCustomFixers/declare_after_opening_tag',
+    //                     'PhpCsFixerCustomFixers/isset_to_array_key_exists',
+    //                     'PhpCsFixerCustomFixers/no_commented_out_code',
+    //                     'PhpCsFixerCustomFixers/phpdoc_only_allowed_annotations',
+    //                     'PhpCsFixerCustomFixers/phpdoc_var_annotation_to_assert',
+    //                 ],
+    //                 true
+    //             )
+    //         ) {
+    //             return $rules;
+    //
+    //         }
+    //
+    //         $rules[$fixer->getName()] = true;
+    //
+    //         return $rules;
+    //     },
+    //     []
+    // )))
+    ->withRules(Rules::fromArray([
         '@PHP70Migration' => true,
         '@PHP70Migration:risky' => true,
         '@PHP71Migration' => true,
@@ -37,8 +83,8 @@ $ruleSet = Config\RuleSet\Php74::create()
         '@PHP73Migration' => true,
         '@PHP74Migration' => true,
         '@PHP74Migration:risky' => true,
-        // '@PHP80Migration' => true,
-        // '@PHP80Migration:risky' => true,
+        '@PHP80Migration' => true,
+        '@PHP80Migration:risky' => true,
         // '@PHP81Migration' => true,
         // '@PHP82Migration' => true,
         // '@PHP83Migration' => true,
@@ -48,6 +94,11 @@ $ruleSet = Config\RuleSet\Php74::create()
         // '@DoctrineAnnotation' => true,
         // '@PhpCsFixer' => true,
         // '@PhpCsFixer:risky' => true,
+    ]))
+    ->withRules(Rules::fromArray([
+        'attribute_empty_parentheses' => [
+            'use_parentheses' => false,
+        ],
         'blank_line_before_statement' => [
             'statements' => [
                 'break',
@@ -91,7 +142,33 @@ $ruleSet = Config\RuleSet\Php74::create()
         'explicit_string_variable' => false,
         'final_class' => false,
         // 'final_internal_class' => false,
-        // 'final_public_method_for_abstract_class' => false,
+        'final_public_method_for_abstract_class' => false,
+        'fully_qualified_strict_types' => [
+            'import_symbols' => false,
+            'leading_backslash_in_global_namespace' => false,
+            'phpdoc_tags' => [
+                // 'param',
+                // 'phpstan-param',
+                // 'phpstan-property',
+                // 'phpstan-property-read',
+                // 'phpstan-property-write',
+                // 'phpstan-return',
+                // 'phpstan-var',
+                // 'property',
+                // 'property-read',
+                // 'property-write',
+                // 'psalm-param',
+                // 'psalm-property',
+                // 'psalm-property-read',
+                // 'psalm-property-write',
+                // 'psalm-return',
+                // 'psalm-var',
+                // 'return',
+                // 'see',
+                // 'throws',
+                // 'var',
+            ],
+        ],
         'logical_operators' => false,
         'mb_str_functions' => false,
         'native_function_invocation' => [
@@ -103,6 +180,9 @@ $ruleSet = Config\RuleSet\Php74::create()
         'new_with_parentheses' => [
             'anonymous_class' => false,
             'named_class' => false,
+        ],
+        'ordered_traits' => [
+            'case_sensitive' => true,
         ],
         'php_unit_data_provider_name' => [
             'prefix' => 'provide',
@@ -136,28 +216,54 @@ $ruleSet = Config\RuleSet\Php74::create()
         ],
         'phpdoc_order' => [
             'order' => [
+                'noinspection',
+                'phan-suppress',
+                'phpcsSuppress',
+                'phpstan-ignore',
+                'psalm-suppress',
+
                 'deprecated',
                 'internal',
                 'covers',
                 'uses',
                 'dataProvider',
-                'noinspection',
-                'psalm-suppress',
                 'param',
                 'throws',
                 'return',
             ],
         ],
+        'phpdoc_order_by_value' => [
+            'annotations' => [
+                'author',
+                'covers',
+                'coversNothing',
+                'dataProvider',
+                'depends',
+                'group',
+                'internal',
+                // 'method',
+                'mixin',
+                'property',
+                'property-read',
+                'property-write',
+                'requires',
+                'throws',
+                'uses',
+            ],
+        ],
         'phpdoc_to_param_type' => [
             'scalar_types' => true,
+            // 'types_map' => [],
             'union_types' => true,
         ],
         'phpdoc_to_property_type' => [
             'scalar_types' => true,
+            // 'types_map' => [],
             'union_types' => true,
         ],
         'phpdoc_to_return_type' => [
             'scalar_types' => true,
+            // 'types_map' => [],
             'union_types' => true,
         ],
         'simplified_if_return' => true,
@@ -167,50 +273,34 @@ $ruleSet = Config\RuleSet\Php74::create()
             'stick_comment_to_next_continuous_control_statement' => true,
         ],
         'static_lambda' => false, // pest
-    ]));
-
-$ruleSet->withCustomFixers(Config\Fixers::fromFixers(
-    ...array_filter(
-        iterator_to_array(new PhpCsFixerCustomFixers\Fixers),
-        static fn (
-            PhpCsFixerCustomFixers\Fixer\AbstractFixer $fixer
-        ): bool => !$fixer instanceof PhpCsFixer\Fixer\DeprecatedFixerInterface
-            && !\array_key_exists($fixer->getName(), $ruleSet->rules()->toArray())
-    )
-));
-
-return Config\Factory::fromRuleSet($ruleSet)
+    ])))
+    ->setUsingCache(true)
+    ->setCacheFile(__DIR__.'/.build/php-cs-fixer/.php-cs-fixer.cache')
     ->setFinder(
         Finder::create()
-            ->in(__DIR__)
+            ->in([
+                __DIR__.'/benchmarks/',
+                __DIR__.'/bin/',
+                __DIR__.'/src/',
+                __DIR__.'/tests/',
+            ])
             ->exclude([
-                '.build/',
-                '.chglog/',
-                '.github/',
-                '__snapshots__/',
-                'build/',
-                'docs/',
-                'node_modules/',
-                'vendor-bin/',
-            ])
-            ->append(glob(__DIR__.'/{*,.*}.php', \GLOB_BRACE))
-            ->append([
-                __DIR__.'/composer-updater',
-                __DIR__.'/platform-lint',
-            ])
-            ->notPath([
-                'bootstrap/*',
-                'storage/*',
-                'resources/view/mail/*',
-                'vendor-bin/*',
+                '__snapshots__',
+                'Fixtures',
             ])
             ->notName([
                 '*.blade.php',
-                // '_ide_helper.php',
+                'JavascriptRenderer.php',
             ])
-            ->ignoreDotFiles(true)
-            ->ignoreVCS(true)
-    )
-    ->setRiskyAllowed(true)
-    ->setUsingCache(true)
-    ->setCacheFile(__DIR__.'/build/php-cs-fixer/.php-cs-fixer.cache');
+            ->append([
+                ...array_filter(
+                    glob(__DIR__.'/{*,.*}.php', \GLOB_BRACE),
+                    static fn (string $filename): bool => !\in_array($filename, [
+                        __DIR__.'/.phpstorm.meta.php',
+                        // __DIR__.'/_ide_helper.php',
+                        __DIR__.'/_ide_helper_models.php',
+                    ], true)
+                ),
+                __DIR__.'/composer-updater',
+            ])
+    );
